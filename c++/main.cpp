bf1c7894ca92dffc666f9edc030adf9f1668f99b
JaeHoonLim <lim10260@nate.com>
2020-10-11 22:52:37 +0900

    add template method
    템플릿 파라미터를 실제 형식으로 바꾸는 작업은 인스턴스화(instantiation)라고 한다. 이를 통해 템플릿의 인스턴스(instance)가 생성된다.

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,5 +1,19 @@
+#include "max1.hpp"
+#include <iostream>
+#include <string>
 
 int main() {
 
+	int i = 42;
+	std::cout << "max(7, i):" << ::max(7, i) << '\n';
+
+	double f1 = 3.4;
+	double f2 = -6.7;
+	std::cout << "max(f1, f2):" << ::max(f1, f2) << '\n';
+
+/*
+max() 템플릿을 호출할 때마다 ::으로 한정했다는 것을 눈여겨보자. 이렇게 표시하면 전역 네임스페이스에 우리의 max() 템플릿을 찾는다.
+퓨준 라이브러리에도 std::max() 템플릿이 있기 때문에 ::로 한정하지 않으면 (오버로딩 해석 & C++의 룩업법칙) 에서  모호하게 될 수도 있다.
+*/
 	return 0;
 }
