1d6ed1529d3e05a254bb3cf4182fda1fffe063ae
Jae Hoon, Lim <lim10260@nate.com>
2020-10-31 20:44:36 +0900

    source arranged

--- source/main.cpp
+++ source/main.cpp
@@ -2,33 +2,9 @@
 #include <string>
 using namespace std;
 
-/*
-* RAII 이 적용된 class 
-*/
-
-class FileRAII {
-public:
-	FileRAII(const string& path, const string& mode) {
-		this->file = std::fopen(path.c_str(), mode.c_str());
-	}
-
-	// 소멸자에서 instance 를 해제하게함
-	~FileRAII() {
-		std::fclose(file);
-	}
-
-	std::FILE* file;
-};
-
 int main(int argc, char** argv)
 {
-	// stack 에 할당
-	FileRAII file("test.txt", "wb+");
-	
-	void* buff = std::malloc(4);
-	if (buff == nullptr) {
-		return -1;
-	}
+
 	
 	return 0;
 }
